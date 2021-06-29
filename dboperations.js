fc397abee41f1006eada1310629695cc150fc457
Tomislav <tsertic5@gmail.com>
2021-06-29 19:59:19 +0200

    added route for search by id

--- dboperations.js
+++ dboperations.js
@@ -5,14 +5,24 @@ const config=require('./dbconfig'),
 const getPersons=async ()=>{
     try {
         let pool=await sql.connect(config)
-        let persons=await pool.request().query("SELECT * from afm.persons") 
-         return persons.recordsets;
+        let persons=await pool.request().query("SELECT LastName from afm.persons") 
+        console.log(persons.recordsets[0])
+         return persons.recordsets[0];
        
     } catch (error) {   
         console.error(error)
     }
 }
-
+const getPersonById=async (id)=>{
+    try {
+        let pool=await sql.connect(config);
+        let product=await pool.request().input('input_parameter',sql.Int,id).query('SELECT * from afm.persons where PersonId = @input_parameter')
+        return product.recordsets[0]
+    } catch (error) {
+        console.error(error)
+    }
+}
 module.exports={
-    getPersons: getPersons
+    getPersons: getPersons,
+    getPersonById: getPersonById
 }
--- server.js
+++ server.js
@@ -1,11 +1,43 @@
 if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config()}
 
-    const dboperations = require('./dboperations')
     const Db=require ('./dboperations'),
-        Persons=require('./persons')
+        Persons=require('./persons'),
+        bodyParser=require('body-parser'),
+        express=require('express'),
+        app=express(),
+        router=express.Router(),
+        cors=require('cors')
     
-    
-    dboperations.getPersons().then(result=>{
-        console.log(result)
+const {urlencoded,json}=bodyParser;
+app.use(urlencoded({extended:true}))
+app.use(json());
+app.use(cors());
+app.use('/api',router)
+
+router.use((request,response,next)=>{
+    console.log('middleware');
+    next();
+})
+
+router.route('/persons').get((request,response)=>{
+
+    Db.getPersons().then(result=>{
+        response.send(result)
     })
+})
+
+router.route('/persons/:id').get((request,response)=>{
+
+    Db.getPersonById(request.params.id).then(result=>{
+        response.send(result)
+    })
+})
+
+
+
+const port=process.env.PORT || 8888;
+app.listen(port,()=>{
+    console.log('DB API is running at port: ' + port)
+});
+
