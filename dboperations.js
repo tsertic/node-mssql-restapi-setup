29b4e15a52004107c77bf1ecf75f165eff34cb77
Tomislav <tsertic5@gmail.com>
2021-07-01 12:05:12 +0200

    added insert route and logic

--- dboperations.js
+++ dboperations.js
@@ -1,28 +1,66 @@
-const config=require('./dbconfig'),
-         sql=require('mssql')
+const config = require("./dbconfig"),
+  sql = require("mssql");
+
+const getPersons = async () => {
+  try {
+    let pool = await sql.connect(config);
+    let persons = await pool
+      .request()
+      .query("SELECT LastName from afm.persons");
+    console.log(persons.recordsets[0]);
+    return persons.recordsets[0];
+  } catch (error) {
+    console.error(error);
+  }
+};
+const getPersonById = async (id) => {
+  try {
+    let pool = await sql.connect(config);
+    let product = await pool
+      .request()
+      .input("input_parameter", sql.Int, id)
+      .query("SELECT * from afm.persons where PersonId = @input_parameter");
+    return product.recordsets[0];
+  } catch (error) {
+    console.error(error);
+  }
+};
+/* Using store procedure  */
+/* 
+    store procedure created in mssql
 
 
-const getPersons=async ()=>{
-    try {
-        let pool=await sql.connect(config)
-        let persons=await pool.request().query("SELECT LastName from afm.persons") 
-        console.log(persons.recordsets[0])
-         return persons.recordsets[0];
-       
-    } catch (error) {   
-        console.error(error)
-    }
-}
-const getPersonById=async (id)=>{
-    try {
-        let pool=await sql.connect(config);
-        let product=await pool.request().input('input_parameter',sql.Int,id).query('SELECT * from afm.persons where PersonId = @input_parameter')
-        return product.recordsets[0]
-    } catch (error) {
-        console.error(error)
-    }
-}
-module.exports={
-    getPersons: getPersons,
-    getPersonById: getPersonById
-}
+    create PROCEDURE [afm].[InsertPersons]
+@PersonID Int, 
+@FirstName varchar(255), 
+@LastName varchar(255) 
+AS
+BEGIN
+INSERT INTO afm.persons (PersonID, FirstName,LastName) VALUES 
+                (@PersonID, @FirstName, @LastName)
+END
+
+
+*/
+const addPersonStoreP = async (person) => {
+  try {
+    const pool = await sql.connect(config);
+    const insertPerson = await pool
+      .request()
+      .input("PersonID", sql.Int, person.PersonID)
+      .input("FirstName", sql.NVarChar, person.FirstName)
+      .input("LastName", sql.NVarChar, person.LastName)
+      .execute("afm.InsertPersons");
+
+    /* Returns status, idealy woudl be insertPerson.recordsets but it returns empty array ,sometnihg must be added to store procedure to return inserted data */
+    return insertPerson;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = {
+  getPersons: getPersons,
+  getPersonById: getPersonById,
+  addPersonStoreP: addPersonStoreP,
+};
--- server.js
+++ server.js
@@ -1,43 +1,49 @@
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config()}
+const { response } = require("express");
 
-    const Db=require ('./dboperations'),
-        Persons=require('./persons'),
-        bodyParser=require('body-parser'),
-        express=require('express'),
-        app=express(),
-        router=express.Router(),
-        cors=require('cors')
-    
-const {urlencoded,json}=bodyParser;
-app.use(urlencoded({extended:true}))
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const Db = require("./dboperations"),
+  Persons = require("./persons"),
+  bodyParser = require("body-parser"),
+  express = require("express"),
+  app = express(),
+  router = express.Router(),
+  cors = require("cors");
+
+const { urlencoded, json } = bodyParser;
+app.use(urlencoded({ extended: true }));
 app.use(json());
 app.use(cors());
-app.use('/api',router)
+app.use("/api", router);
 
-router.use((request,response,next)=>{
-    console.log('middleware');
-    next();
-})
-
-router.route('/persons').get((request,response)=>{
-
-    Db.getPersons().then(result=>{
-        response.send(result)
-    })
-})
-
-router.route('/persons/:id').get((request,response)=>{
-
-    Db.getPersonById(request.params.id).then(result=>{
-        response.send(result)
-    })
-})
-
-
-
-const port=process.env.PORT || 8888;
-app.listen(port,()=>{
-    console.log('DB API is running at port: ' + port)
+router.use((request, response, next) => {
+  console.log("middleware");
+  next();
 });
 
+router.route("/persons").get((request, response) => {
+  Db.getPersons().then((result) => {
+    response.send(result);
+  });
+});
+
+router.route("/persons/:id").get((request, response) => {
+  Db.getPersonById(request.params.id).then((result) => {
+    response.send(result);
+  });
+});
+
+router.route("/persons").post((req, res) => {
+  let person = { ...req.body };
+
+  Db.addPersonStoreP(person).then((result) => {
+    res.status(201).json(result);
+  });
+});
+
+const port = process.env.PORT || 8888;
+app.listen(port, () => {
+  console.log("DB API is running at port: " + port);
+});
